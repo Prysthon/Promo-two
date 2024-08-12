ab524021fa5fb6b4595d025bab3fe7d365b19b75
Tiago Prysthon <tiagoprysthon14@gmail.com>
2024-08-12 11:06:43 -0300

    [add] nova página de Profile

--- mobile-app/App.js
+++ mobile-app/App.js
@@ -1,10 +1,37 @@
 import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
 import Login from './screens/Login.js'; 
 import Register from './screens/Register';
+import Profile from './screens/Profile.js';
+
+import { Button } from 'react-native';
 
 const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
+
+function HomeTabs({ navigation }) {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen 
+        name="Perfil" 
+        component={Profile} 
+        options={{
+          headerRight: () => (
+            <Button
+              onPress={() => navigation.replace('Login')}
+              title="Logout"
+              color="#000"
+            />
+          ),
+        }}
+      />
+      {/* Você pode adicionar outras abas aqui, como Meu Perfil, Avaliações, Configurações */}
+    </Tab.Navigator>
+  );
+}
 
 export default function App() {
   return (
@@ -19,6 +46,11 @@ export default function App() {
           name="Register" 
           component={Register}
           options={{ headerShown: false }}
+        />
+        <Stack.Screen 
+          name="HomeTabs" 
+          component={HomeTabs} 
+          options={{ headerShown: false }} 
         />
       </Stack.Navigator>
     </NavigationContainer>
---
+++ mobile-app/screens/Profile.js
@@ -0,0 +1,98 @@
+import React from 'react';
+import { View, Text, StyleSheet, Image, TouchableOpacity } from 'react-native';
+
+export default function Profile({ navigation }) {
+  return (
+    <View style={styles.container}>
+      <View style={styles.profileContainer}>
+        <Image 
+          source={{ uri: 'https://via.placeholder.com/150' }} 
+          style={styles.profileImage}
+        />
+        <Text style={styles.profileName}>John Doe</Text>
+        <Text style={styles.profileEmail}>john.doe@example.com</Text>
+      </View>
+      <View style={styles.optionsContainer}>
+        <TouchableOpacity style={styles.option}>
+          <Text style={styles.optionText}>Editar Perfil</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.option}>
+          <Text style={styles.optionText}>Configurações</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.option}>
+          <Text style={styles.optionText}>Histórico de Compras</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.option}>
+          <Text style={styles.optionText}>Pagamentos</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.option}>
+          <Text style={styles.optionText}>Endereços</Text>
+        </TouchableOpacity>
+        <TouchableOpacity 
+          style={styles.logoutButton} 
+          onPress={() => navigation.replace('Login')}
+        >
+          <Text style={styles.logoutText}>Logout</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#ffffff',
+    alignItems: 'center',
+    paddingTop: 20,
+  },
+  header: {
+    marginBottom: 20,
+  },
+  profileContainer: {
+    alignItems: 'center',
+    marginBottom: 30,
+  },
+  profileImage: {
+    width: 100,
+    height: 100,
+    borderRadius: 50,
+    marginBottom: 10,
+  },
+  profileName: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: '#261f1f',
+    marginBottom: 5,
+  },
+  profileEmail: {
+    fontSize: 16,
+    color: '#7f8c8d',
+  },
+  optionsContainer: {
+    width: '80%',
+  },
+  option: {
+    backgroundColor: '#3ad3f3',
+    padding: 15,
+    borderRadius: 10,
+    marginBottom: 15,
+    alignItems: 'center',
+  },
+  optionText: {
+    color: '#fff',
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+  logoutButton: {
+    backgroundColor: '#227f91',
+    padding: 15,
+    borderRadius: 10,
+    alignItems: 'center',
+  },
+  logoutText: {
+    color: '#fff',
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+});
